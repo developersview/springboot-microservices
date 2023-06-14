e21ab1a3d6f581c954fe9e7c8713a87e33f5f67d
developersview <pcslg1998@gmail.com>
2023-06-14 12:20:13 +0530

    user controller class

---
+++ restful-web-services/src/main/java/com/developersview/webservices/restfulwebservices/user/UserController.java
@@ -0,0 +1,51 @@
+package com.developersview.webservices.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+/**
+ * @author pranoy.chakraborty
+ * @Date 14/06/2023
+ */
+@RestController
+public class UserController {
+
+    @Autowired
+    public UserDaoService userDaoService;
+
+    public UserController(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    //GET all users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    //GET user by id
+    @GetMapping("/users/{id}")
+    public User retrieveUserById(@PathVariable int id) {
+        User user = userDaoService.findById(id);
+        if (user == null) {
+            throw new UserNotFoundException("Id : " + id);
+        }
+        return user;
+    }
+
+    //POST new user
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
